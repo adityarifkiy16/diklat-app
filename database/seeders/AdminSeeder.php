13108de457bf9cb639daf06b1293dffdd508d832
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-07-08 21:55:06 +0700

    seeding database

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'username' => 'administrator',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+            'role_id' => 1
+        ]);
+    }
+}
