4266dee7a02a1f5ec1b81670ab2c39d3b81ff3a7
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-08-06 21:30:53 +0700

    feat : add button menu instruktur

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -27,13 +27,14 @@
                 </li>
                 <li class="nav-item"><a href="{{url('/user')}}" class="nav-link"><i class="icon-user"></i><span>Master User</span></a></li>
                 <li class="nav-item"><a href="{{url('/peserta')}}" class="nav-link"><i class="icon-user"></i><span>Master Peserta</span></a></li>
+                <li class="nav-item"><a href="{{url('/instruktur')}}" class="nav-link"><i class="icon-user"></i><span>Master Instruktur</span></a></li>
                 <li class="nav-item"><a href="{{url('/diklat')}}" class="nav-link"><i class="icon-task"></i><span>Master Diklat</span></a></li>
                 <li class="nav-item-header">
                     <div class="text-uppercase font-size-xs line-height-xs">Lainnya</div> <i class="icon-menu"></i>
                 </li>
 
-                <li class="nav-item"><a href="{{url('/pendaftaran')}}" class="nav-link"><i class="icon-user"></i><span>Pendaftaran</span></a></li>
-                <li class="nav-item"><a href="{{url('/penjadwalan')}}" class="nav-link"><i class="icon-user"></i><span>Penjadwalan</span></a></li>
+                <li class="nav-item"><a href="{{url('/pendaftaran')}}" class="nav-link"><i class="icon-task"></i><span>Pendaftaran</span></a></li>
+                <li class="nav-item"><a href="{{url('/penjadwalan')}}" class="nav-link"><i class="icon-task"></i><span>Penjadwalan</span></a></li>
                 </li>
             </ul>
         </div>
