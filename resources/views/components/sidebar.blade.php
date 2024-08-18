6ba18d778c0045dd170016367755f329f1fc8e3e
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-08-18 20:36:44 +0700

    refactor: adding link dashboard and pendaftaran pages

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -21,7 +21,7 @@
         <!-- Main navigation -->
         <div class="card card-sidebar-mobile">
             <ul class="nav nav-sidebar" data-nav-type="accordion">
-                <li class="nav-item"><a href="index.html" class="nav-link"><i class="icon-home"></i><span>Dashboard</span></a></li>
+                <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link"><i class="icon-home"></i><span>Dashboard</span></a></li>
                 <li class="nav-item-header">
                     <div class="text-uppercase font-size-xs line-height-xs">Menu</div> <i class="icon-menu"></i>
                 </li>
@@ -33,7 +33,7 @@
                     <div class="text-uppercase font-size-xs line-height-xs">Lainnya</div> <i class="icon-menu"></i>
                 </li>
 
-                <li class="nav-item"><a href="{{url('/pendaftaran')}}" class="nav-link"><i class="icon-task"></i><span>Pendaftaran</span></a></li>
+                <li class="nav-item"><a href="{{ route('pendaftaran') }}" class="nav-link"><i class="icon-task"></i><span>Pendaftaran</span></a></li>
                 <li class="nav-item"><a href="{{url('/penjadwalan')}}" class="nav-link"><i class="icon-task"></i><span>Penjadwalan</span></a></li>
                 </li>
             </ul>
